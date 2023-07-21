f66d391cc16ad5d928b3a073c2c6b9b8c1f62fe9
777Hemant <hemantmishra12012000@gmail.com>
2023-07-22 01:01:27 +0530

    sfah

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -20,22 +20,22 @@ const Cart = () => {
   };
 
   return (
-    <div className="p-4 w-full">
+    <div className="p-4 max-w-screen-sm">
       {cart.length === 0 ? (
         <div className="text-gray-600 py-8 my-40 text-3xl flex justify-center items-center">
           There is no product added to the cart.
         </div>
       ) : (
-        <div className="max-w-3xl grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="max-w-fit grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {cart.map((post) => (
             <div key={post.id} className="p-4 border border-gray-300 rounded-lg shadow-md">
-              <img src={post.image} alt={post.title} className="w-full h-48 object-cover max-w-full h-auto" />
+              <img src={post.image} alt={post.title} className="object-cover max-w-full h-auto" />
               <h3 className="text-gray-900 font-semibold text-xl my-2">{post.title}</h3>
               <div className="text-2xl text-red-800">${post.price}</div>
               <AiOutlineDelete size={30} onClick={() => handleRemoveItem(post.id)} />
             </div>
           ))}
-          <div className="container bg-green-200 text-black w-full text-2xl p-10">
+          <div className="container bg-green-200 text-black min-w-fit text-2xl p-10">
             <p className="font-serif text-black">
               Total items you have bought:<span className="ml-8 bg-slate-400 font-serif">{cart.length}</span>
             </p>
