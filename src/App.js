0a8711df142584f4121293010733670a7dba8082
777Hemant <hemantmishra12012000@gmail.com>
2023-07-21 22:30:29 +0530

    sdjhsf

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ const App = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <Home/>
       <Routes>
         <Route path="*" element={<Home/>}></Route>
         <Route path="/Products" element={<Products/>}></Route>
